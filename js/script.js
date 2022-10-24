a4666338c5be46d475de15113fc14a6aad8f9e05
Aleksander Skula <aleksander.skula@gmail.com>
2022-10-24 19:29:21 +0200

    functions improvement

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 {
+        const mainButton = document.querySelector(".js-mainButton");
+        const image = document.querySelector(".js-image");
+        const buttonHide = document.querySelector(".js-buttonHide");
+
     const welcome = () => {
         console.log("Witaj deweloperze!");
     }
@@ -10,10 +14,8 @@
         const navigation = document.querySelector(".js-nav");
         const navigationLink = document.querySelector(".js-navLink");
         const mainElement = document.querySelector(".js-main");
-        const mainButtonColor = document.querySelector(".js-mainButton");
         const occupation = document.querySelector(".js-occupation");
         const dropcap = document.querySelector(".js-dropcap");
-        const image = document.querySelector(".js-image");
         const tableCaption = document.querySelector(".js-caption");
         const tableCell = document.querySelector(".js-tableCell");
         bodyElement.classList.toggle("body--contrast");
@@ -21,7 +23,7 @@
         bodySubHeader.classList.toggle("body__subHeader--contrast");
         navigation.classList.toggle("navigation--contrast");
         navigationLink.classList.toggle("navigation__link--contrast");
-        mainButtonColor.classList.toggle("main__button--contrast");
+        mainButton.classList.toggle("main__button--contrast");
         mainElement.classList.toggle("main--contrast");
         occupation.classList.toggle("article__occupation--contrast");
         dropcap.classList.toggle("article__dropcap--contrast");
@@ -32,32 +34,28 @@
 
     const onChangeTextClick = () => {
 
-        const mainButtonText = document.querySelector(".js-mainButton");
-
-        if (mainButtonText.classList.contains("main__button--contrast")) {
-            mainButtonText.innerText = "NORMALNY WIDOK STRONY";
+        if (mainButton.classList.contains("main__button--contrast")) {
+            mainButton.innerText = "NORMALNY WIDOK STRONY";
         } else {
-            mainButtonText.innerText = "WYSOKI KONTRAST STRONY";
+            mainButton.innerText = "WYSOKI KONTRAST STRONY";
         }
     }
 
     const imageHide = () => {
 
-        const imageText = document.querySelector(".js-image");
-        const buttonHideText = document.querySelector(".js-buttonHide");
+        const buttonHide = document.querySelector(".js-buttonHide");
 
-        imageText.classList.toggle("main__imageHide")
+        image.classList.toggle("main__imageHide")
 
-        if (imageText.classList.contains("main__imageHide")) {
-            buttonHideText.innerText = "POKAŻ ZDJĘCIE"
+        if (image.classList.contains("main__imageHide")) {
+            buttonHide.innerText = "POKAŻ ZDJĘCIE"
         } else {
-            buttonHideText.innerText = "UKRYJ ZDJĘCIE"
+            buttonHide.innerText = "UKRYJ ZDJĘCIE"
         }
     }
 
     const init = () => {
 
-        const buttonHide = document.querySelector(".js-buttonHide");
         const mainButton = document.querySelector(".js-mainButton");
 
         mainButton.addEventListener("click", onChangeBackgroundClick);
